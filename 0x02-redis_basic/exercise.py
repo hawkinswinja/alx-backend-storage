52682a50302f2c97d400cd34686f250e21dd079a
hawkinswinja <hawkinswinja@gmail.com>
2023-04-14 19:15:44 +0000

    pep8 validation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -57,7 +57,8 @@ class Cache():
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+    def get(self, key: str,
+            fn: Callable = None) -> Union[str, bytes, int, float]:
         """returns the value of key"""
         val = self._redis.get(key)
         if fn:
